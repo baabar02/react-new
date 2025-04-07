0302f414f9391bf5b353a03765040262713d72e8
baabar02 <baabarmx@gmail.com>
2025-04-07 20:35:50 +0800

    new

---
+++ src/components/Button.jsx
@@ -0,0 +1,26 @@
+// src/components/Button.jsx
+import React from 'react';
+// import ''; //
+
+const Button = (props) => {
+  return (
+    <button
+      className="custom-button"
+      style={{
+        backgroundColor: props.backgroundColor,
+        color: props.color,
+        width: props.width,
+        height: "48px",
+        borderRadius: "4px",
+        border: "none",
+        marginLeft: props.marginLeft,
+        cursor: "pointer",
+        textAlign: "center"
+      }}
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
